1864479c461ba76194d0b75e422bcd49801fac4c
HaldenMemphis <jasondennis12138@gmail.com>
2023-06-30 10:46:30 +0100

    -- Story: Initial Spring Project -- Author: Yifan Liu

---
+++ src/main/java/com/msc/kaapatch/KaaPatchApplication.java
@@ -0,0 +1,13 @@
+package com.msc.kaapatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaaPatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaaPatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msc/kaapatch/KaaPatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.kaapatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaaPatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
